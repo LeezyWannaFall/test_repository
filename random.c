a3867ca21e32759edd362cf3949c15f5318a5968
Leezy <leezy@ubuntu22.myguest.virtualbox.org>
2024-08-06 23:04:03 +0300

    sdfsd

---
+++ random.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    int wtf = 0;
+    if (scanf("%d", &wtf) == 1 && c != '\n') {
+        printf("%d\n", wtf);
+    } else {
+        printf("n/a\n");
+    }
+}
