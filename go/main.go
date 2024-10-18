3639cc5087101390257f14fb4c01f72dd2f284c7
Leezy <191919qaz@gmail.com>
2024-10-18 03:07:01 +0300

    switch ex

--- go/main.go
+++ go/main.go
@@ -2,12 +2,39 @@ package main
 
 import (
 	"fmt"
-	
+
 )
 
 func main() {
-	var x, z float64
-	fmt.Scan(&x, &z)	
-	z -= (z*z - x) / (2*z)
-	fmt.Println(x, z)
+	var x, y float64
+	var operator string
+	fmt.Println("Введите первое число: ")
+	fmt.Scan(&x)
+	fmt.Println("Введите второе число: ")
+	fmt.Scan(&y)
+	fmt.Println("Введите оператор (+, -, *, /, %): ")
+	fmt.Scan(&operator)
+
+	switch operator {
+	case "+":
+		fmt.Println("Результат:", x + y)
+	case "-":
+		fmt.Println("Результат:", x - y)
+	case "*":
+		fmt.Println("Результат:", x * y)
+	case "/":
+		if y == 0 {
+			fmt.Println("Ошибка, делить на 0 нельзя!")
+		} else {
+			fmt.Println("Результат:", x / y)
+		}
+	case "%":
+		if y == 0 {
+			fmt.Println("Ошибка: деление на ноль!")
+		} else {
+			fmt.Println("Результат:", int(x) % int(y))
+		}
+	default:
+		fmt.Println("Неизвестный оператор")
+	}
 }
