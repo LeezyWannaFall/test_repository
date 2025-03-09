ad4baf5e54a95f619cc210ade0df1a31b848cb06
pc ubuntu <191919qaz@gmail.com>
2025-03-09 15:37:16 +0300

    s21_create_matrix add

--- matrix_src/s21_matrix.c
+++ matrix_src/s21_matrix.c
@@ -1,8 +1,32 @@
 #include "s21_matrix.h"
 
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
-    matrix_t mat;
+  // проверка на корректность входных данных
+  if (rows <= 0 || columns <= 0 || result == NULL) return INCORRECT_MATRIX;
 
-    
+  // инициализация струткуры
+  result->rows = rows;
+  result->columns = columns;
 
+  // выделение памяти под матрицу
+  result->matrix = (double **)malloc(rows * sizeof(double *));
+
+  // проверка на NULL
+  if (!result) return INCORRECT_MATRIX;
+
+  // выделение памяти под каждый столбец
+  for (int i = 0; i < rows; i++) {
+    result->matrix[i] = (double *)malloc(columns * sizeof(double));
+    // очистка уже выделенной памяти в случае ошибки
+    if (!result->matrix[i]) {
+      for (int j = 0; j < i; i++) {
+        free(result->matrix[j]);
+      }
+
+      free(result->matrix);
+      return INCORRECT_MATRIX;
+    }
+  }
+  
+  return OK;
 }
