35c62d30882d4b449ba06e7928feb2b0c9cac2ab
Leezy <191919qaz@gmail.com>
2025-03-10 21:51:52 +0300

    add s21_eq

--- matrix_src/core/s21_matrix.c
+++ matrix_src/core/s21_matrix.c
@@ -12,14 +12,14 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
   result->matrix = (double **)malloc(rows * sizeof(double *));
 
   // проверка на NULL
-  if (!result) return INCORRECT_MATRIX;
+  if (!result->matrix) return INCORRECT_MATRIX;
 
   // выделение памяти под каждый столбец
   for (int i = 0; i < rows; i++) {
-    result->matrix[i] = (double *)malloc(columns * sizeof(double));
+    result->matrix[i] = (double *)calloc(columns, sizeof(double));
     // очистка уже выделенной памяти в случае ошибки
     if (!result->matrix[i]) {
-      for (int j = 0; j < i; i++) {
+      for (int j = 0; j < i; j++) {
         free(result->matrix[j]);
       }
 
@@ -42,4 +42,18 @@ void s21_remove_matrix(matrix_t *A) {
     A->matrix = NULL;
     A->columns = 0;
     A->rows = 0;
+}
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B) {
+  if (!A || A->matrix || !B || B->matrix) return FAILURE;
+
+  if (A->rows != B->rows || A->columns != B->columns) return FAILURE;
+
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->columns; j++) {
+      if (A->matrix[i][j] != B->matrix[i][j]) return FAILURE;
+    }
+  }
+
+  return SUCCESS;
 }
