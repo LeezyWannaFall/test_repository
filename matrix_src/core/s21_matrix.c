0b1dda35401f5842c07a893e670a6055b75a79d6
pc ubuntu <191919qaz@gmail.com>
2025-03-11 08:24:07 +0300

    add comments

--- matrix_src/core/s21_matrix.c
+++ matrix_src/core/s21_matrix.c
@@ -32,20 +32,21 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
 }
 
 void s21_remove_matrix(matrix_t *A) {
-    //  проверка на корректность входных данных
-    if (!A || !A->matrix) return INCORRECT_MATRIX;
+  //  проверка на корректность входных данных
+  if (!A || !A->matrix) return;
 
-    // очищение памяти
-    for (int i = 0; i < A->rows; i++) free(A->matrix[i]);
-    free(A->matrix);
-
-    A->matrix = NULL;
-    A->columns = 0;
-    A->rows = 0;
+  // очищение памяти
+  for (int i = 0; i < A->rows; i++) free(A->matrix[i]);
+  free(A->matrix);
+  
+  // Обнуление полей структуры
+  A->matrix = NULL;
+  A->columns = 0;
+  A->rows = 0;
 }
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
-  if (!A || A->matrix || !B || B->matrix) return FAILURE;
+  if (!A || !A->matrix || !B || !B->matrix) return FAILURE;
 
   if (A->rows != B->rows || A->columns != B->columns) return FAILURE;
 
@@ -59,15 +60,21 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
 }
 
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-  if (!A || A->matrix || !B || B->matrix || !result || !result->matrix) return INCORRECT_MATRIX;
+    // Проверка на NULL
+    if (!A || !A->matrix || !B || !B->matrix || !result) return INCORRECT_MATRIX;
 
-  if (A->rows != B->rows || A->columns != B->columns) return INCORRECT_MATRIX;
+    // Проверка на совпадение размеров
+    if (A->rows != B->rows || A->columns != B->columns) return INCORRECT_MATRIX;
 
-  for (int i = 0; i < A->rows; i++) {
-    for (int j = 0; j < A->columns; j++) {
-      result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
+    // Создаём матрицу `result`
+    if (s21_create_matrix(A->rows, A->columns, result) != OK) return INCORRECT_MATRIX;
+
+    // Складываем элементы матрицы
+    for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+            result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
+        }
     }
-  }
 
-  return OK;
+    return OK;
 }
