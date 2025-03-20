50b1baf26eb83827d382119dc5298a395b8ba378
Leezy <191919qaz@gmail.com>
2025-03-20 22:54:43 +0300

    part2

---
+++ part2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hostname=$(cat /etc/hostname)
+timezone=$(cat /etc/timezone)
+os=$(cat /etc/issue)
+uptime=$(uptime -s)
+uptime_sec=$()
+
+echo "HOSTNAME: $hostname"
+echo "TIMEZONE: $timezone"
+echo "USER: $USER"
+echo "OS: $os"
+echo "UPTIME: $uptime"
+echo "UPTIME_SEC: $uptime_sec"
