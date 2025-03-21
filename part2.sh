847c2b15808f77b8ad5d4850ca88c8f1966f4207
pc ubuntu <191919qaz@gmail.com>
2025-03-22 01:14:48 +0300

    to do ip

--- part2.sh
+++ part2.sh
@@ -3,8 +3,9 @@
 hostname=$(cat /etc/hostname)
 timezone=$(cat /etc/timezone)
 os=$(cat /etc/issue)
-uptime=$(uptime -s)
+uptime=$(uptime -p)
 uptime_sec=$(( $(date +%s) - $(date -d "$(uptime -s)" +%s) ))
+ip=$(ip -4 addr show enp3s0 | awk -F: '{print $5}') # to do
 
 echo "HOSTNAME: $hostname"
 echo "TIMEZONE: $timezone"
@@ -12,3 +13,4 @@ echo "USER: $USER"
 echo "OS: $os"
 echo "UPTIME: $uptime"
 echo "UPTIME_SEC: $uptime_sec"
+echo "IP: $ip"
