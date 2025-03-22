289ac52562edcd3df90b6a57d18ef1157a358218
pc ubuntu <191919qaz@gmail.com>
2025-03-23 02:45:14 +0300

    to do IPmask

--- part2.sh
+++ part2.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-hostname=$(cat /etc/hostname)
+hostname=$(hostname)
 timezone=$(cat /etc/timezone)
-os=$(cat /etc/issue)
+os=$(cat /etc/issue | cut -d'\' -f1)
+date=$(date +"%d %b %Y %H:%M:%S")
 uptime=$(uptime -p)
 uptime_sec=$(( $(date +%s) - $(date -d "$(uptime -s)" +%s) ))
-ip=$(ip -4 a | awk ' /192/{print $2}')
+ip=$(ip -o -4 a | awk ' {print $4; exit}')
+# to do 
+# mask=$(ip -o -4 a | awk ' {print $4; exit}' | cut -d'/' -f2) 
 
 echo "HOSTNAME: $hostname"
 echo "TIMEZONE: $timezone"
 echo "USER: $USER"
 echo "OS: $os"
+echo "DATE: $date"
 echo "UPTIME: $uptime"
 echo "UPTIME_SEC: $uptime_sec"
 echo "IP: $ip"
+# echo "MASK: $mask"
