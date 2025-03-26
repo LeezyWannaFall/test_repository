c34c9b0d001f1c20c4bb1c45454d7209070a5cd8
Leezy <191919qaz@gmail.com>
2025-03-26 15:46:37 +0300

    123

--- part2.sh
+++ part2.sh
@@ -7,15 +7,17 @@ date=$(date +"%d %b %Y %H:%M:%S")
 uptime=$(uptime -p)
 uptime_sec=$(( $(date +%s) - $(date -d "$(uptime -s)" +%s) ))
 ip=$(ip -o -4 a | awk ' {print $4; exit}')
-# to do 
-# mask=$(ip -o -4 a | awk ' {print $4; exit}' | cut -d'/' -f2) 
+mask=$(ipcalc $ip | grep -e "Netmask:" | cut -d'=' -f1 | cut -d':' -f2)
+# gateway=$(ip r | awk -Fvia ' {print $1}' | cut -d'dev' -f2)
 
-echo "HOSTNAME: $hostname"
-echo "TIMEZONE: $timezone"
-echo "USER: $USER"
-echo "OS: $os"
-echo "DATE: $date"
-echo "UPTIME: $uptime"
-echo "UPTIME_SEC: $uptime_sec"
-echo "IP: $ip"
-# echo "MASK: $mask"
+echo -e "\e[32mHOSTNAME\e[0m: $hostname"
+echo -e "\e[32mTIMEZONE\e[0m: $timezone"
+echo -e "\e[32mUSER\e[0m: $USER"
+echo -e "\e[32mOS\e[0m: $os"
+echo -e "\e[32mDATE\e[0m: $date"
+echo -e "\e[32mUPTIME\e[0m: $uptime"
+echo -e "\e[32mUPTIME_SEC\e[0m: $uptime_sec"
+echo -e "\e[32mIP\e[0m: $ip"
+echo -e "\e[32mMASK\e[0m: $mask"
+echo -e "\e[32mGATEWAY\e[0m: $gateway"
+
