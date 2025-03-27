84de91a5c1da28e023f1bfa29da9266f981a8af6
pc ubuntu <191919qaz@gmail.com>
2025-03-28 00:22:29 +0300

    gateway complete

--- part2.sh
+++ part2.sh
@@ -8,7 +8,8 @@ uptime=$(uptime -p)
 uptime_sec=$(( $(date +%s) - $(date -d "$(uptime -s)" +%s) ))
 ip=$(ip -o -4 a | awk ' {print $4; exit}')
 mask=$(ipcalc $ip | grep -e "Netmask:" | cut -d'=' -f1 | cut -d':' -f2)
-# gateway=$(ip r | awk -Fvia ' {print $1}' | cut -d'dev' -f2)
+gateway=$(ip r | grep -e "default" | awk -Fvia ' {print $2}' | cut -d'd' -f1)
+
 
 echo -e "\e[32mHOSTNAME\e[0m: $hostname"
 echo -e "\e[32mTIMEZONE\e[0m: $timezone"
@@ -20,4 +21,4 @@ echo -e "\e[32mUPTIME_SEC\e[0m: $uptime_sec"
 echo -e "\e[32mIP\e[0m: $ip"
 echo -e "\e[32mMASK\e[0m: $mask"
 echo -e "\e[32mGATEWAY\e[0m: $gateway"
-
+echo -e "\e[32mGATEWAY\e[0m: $
