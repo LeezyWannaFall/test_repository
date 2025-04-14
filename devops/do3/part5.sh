5bc419c0edb999c47ce1886ee7a69b68be48bf38
Leezy <191919qaz@gmail.com>
2025-04-14 15:51:09 +0300

    part5

--- devops/do3/part5.sh
+++ devops/do3/part5.sh
@@ -10,9 +10,19 @@ fi
 directory=$1
 
 if [ ! -d "$directory" ]; then
-    echo "Error: $directory is not valid"
+    echo "Error: $directory is not valid directory"
     exit 1
 fi
 
 #TO DO: main part of script
+
+total_folders=$(find /home/leezy/test_repository/ -type d | wc -l)
+
+top_folders=$(du -h --max-depth=1 "$directory" 2>/dev/null | sort -hr | head -n 6 | tail -n 5 | awk '{printf("%d - %s, %s\n", NR, $2, $1)}')
+
+total_files=$(find $directory -type f | wc -l)
+
+total_conf_files=$(find $directory -type f -name "*.conf" | wc -l)
+
+total_exec_files=$(find "$directory" -type f -exec file --mime-type {} + 2>/dev/null | grep 'text/plain' | wc -l)
 
