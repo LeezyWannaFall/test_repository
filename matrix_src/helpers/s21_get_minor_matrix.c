7fcc65b276b77909c2bff6434e9ea68c743a7f1d
pc ubuntu <191919qaz@gmail.com>
2025-04-27 11:54:42 +0300

    edit get minor function

--- matrix_src/helpers/s21_get_minor_matrix.c
+++ matrix_src/helpers/s21_get_minor_matrix.c
@@ -3,14 +3,16 @@
 // Функция для создания подматрицы, исключая строку row и столбец col 
 void s21_get_minor_matrix(matrix_t *A, int row, int col, matrix_t *minor) {
   int minor_row = 0, minor_col = 0;
+
   for (int i = 0; i < A->rows; i++) {
-    if (i == row) continue;
-    minor_col = 0;
-    for (int j = 0; j < A->columns; j++) {
-      if (j == col) continue;
-      minor->matrix[minor_row][minor_col] = A->matrix[i][j];
-      minor_col++;
+    if (i != row) {
+      for (int j = 0; j < A->columns; j++) {
+        if (j != col) {
+          minor->matrix[minor_row][minor_col++] = A->matrix[i][j];
+        }
+      }
+      minor_row++;
+      minor_col = 0;
     }
-    minor_row++;
   }
 }
