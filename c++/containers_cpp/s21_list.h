05054f2ca63fefdc0ea4a94fd22d52a9f4d6d68b
pc ubuntu <191919qaz@gmail.com>
2025-07-18 22:39:02 +0300

    header is done?

--- c++/containers_cpp/s21_list.h
+++ c++/containers_cpp/s21_list.h
@@ -19,17 +19,53 @@ class list {
   size_type size_;
 
  public:
+  // Class types
   using value_type = T;
   using reference = T &;
+  using iterator = T *;
+  using const_iterator = const T *;
   using const_reference = const T &;
   using size_type = std::size_t;
+
   // Constructors
   list();
-  // ...code...
+  list(size_type n);
+  list(std::initializer_list<value_type> const &items);
+  list(const list &l);
+  list(list &&l);
+  operator=(list &&l);
+
   // Destructor
   ~list();
-  // ...methods (code...)
+
+  // Accesors
+  const_reference front();
+  const_reference back();
+
+  // Iterators
+  iterator begin();
+  iterator end();
+
+  // Capacity
+  bool empty();
+  size_type size();
+  size_type max_size();
+
+  // Methods
+  void clear();
+  iterator insert(iterator pos, const_reference value);
+  void erase(iterator pos);
+  void push_back(const_reference value);
+  void pop_back();
+  void push_front(const_reference value);
+  void pop_front();
+  void swap(list& other);
+  void merge(list& other);
+  void splice(const_iterator pos, list& other);
+  void reverse();
+  void unique();
+  void sort();
 };
 
-}  // namespace s21
+} // namespace s21
 #endif  // S21_LIST_H_
